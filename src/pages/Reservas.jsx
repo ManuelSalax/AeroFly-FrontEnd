3b00fddf74c4d42bee0367f5cbf16c792d23949b
ManuelSalaxx <mm4228027@gmail.com>
2025-11-07 05:45:53 -0500

    update

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -14,8 +14,9 @@ export default function Reservas() {
           className="w-full border border-gray-300 rounded px-4 py-2"
         />
         <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700">
-          Confirmar Reserva
+          Confirmar Reservas
         </button>
+
       </form>
     </div>
   );
